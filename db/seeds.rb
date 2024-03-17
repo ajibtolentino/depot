b09d6143211dc058d847dfcaab942a06f7da96cc
ajibtolentino <ajibtolentino@outlook.com>
2024-03-18 02:39:21 +0800

    Database/Index

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,18 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+Product.delete_all
+#...
+Product.create!(title: 'Design and Build Great Web APIs',
+  description:
+    %{<p>
+      <em>Cetaphil</em>
+      Cetaphil is a skincare brand that has been recommended 
+      by dermatologists for over 70 years, and is today used 
+      by millions of women with sensitive skin from all over
+       the world. The original product, called Cetaphil 
+       Cleansing Lotion, was invented by a pharmacist in 
+       1947 specifically for dermatological needs.
+      </p>},
+  image_url: 'cetaphil.jpeg',
+price: 24.95) #...

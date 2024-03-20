a07feec061ff96adc70cfccdf9b36912e4d4b943
ajibtolentino <ajibtolentino@outlook.com>
2024-03-20 22:46:08 +0800

    Cart Creation

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -16,8 +16,11 @@ class ProductsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  test "should create product" do assert_difference("Product.count") do
-    post products_url, params: { product: {
+
+  test "should create product" do 
+    assert_difference("Product.count") do
+    post products_url, params: { 
+    product: {
     description: @product.description, 
     image_url: @product.image_url, 
     price: @product.price,
@@ -39,22 +42,38 @@ end
   end
 
   test "should update product" do
-    patch product_url(@product), params: { product: {
+    patch product_url(@product), params: { 
+      product: {
     description: @product.description, 
     image_url: @product.image_url, 
     price: @product.price,
     title: @title,
   }
 }
+
 assert_redirected_to product_url(@product)
 end
     
 
-  test "should destroy product" do
-    assert_difference("Product.count", -1) do
-      delete product_url(@product)
-    end
-
+test "can't delete product in cart" do 
+  assert_difference("Product.count", 0) do
+  delete product_url(products(:two)) 
+end
     assert_redirected_to products_url
   end
+  test "should destroy product" do 
+    assert_difference("Product.count", -1) do
+      delete product_url(@product)
+  end
+  assert_redirected_to products_url
+  
 end
+
+test "should create line_item" do assert_difference("LineItem.count") do
+  post line_items_url, params: { product_id: products(:ruby).id } end
+      follow_redirect!
+  assert_select 'h2', 'Your Pragmatic Cart'
+  assert_select 'li', 'Programming Ruby 1.9' end
+end
+
+
